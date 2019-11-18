31cdf54075e03ce49744b2de5eecff827b867b1d
alskaa <alisagorodeckaa7786@gmail.com>
2019-11-19 02:37:13 +0300

    Добавлена возможность сохранения карты

--- IOManager.py
+++ IOManager.py
@@ -3,15 +3,16 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from map_parser import *
+from mapclass import Map
 
 
 
 def open_map(parent: QtWidgets.QWidget):
     input_map = QFileDialog.getOpenFileName(parent, 'Open file', '.', filter=('YAML file (*.yaml)'))[0]
-    parent.tiles.tiles = get_tiles(input_map)
 
-    objects_array = get_objects(input_map)
+    map = Map(tiles_to_objects(get_tiles(input_map)),  map_objects_to_objects(get_objects(input_map)))
+    parent.tiles.tiles = map.tiles
+
     parent.tiles.gridSize = 100*get_tile_size(input_map)
     print(parent.tiles.tiles)
-    print(objects_array)
     print(parent.tiles.gridSize)
