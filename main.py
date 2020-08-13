c266771bfd093404a5240b65d94f6e8e44ce5605
Dmitry Ivanov <darcenrall@gmail.com>
2020-08-13 12:47:56 +0300

    Add translator on starting

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QTranslator
 from mainwindow import duck_window
 
+LANG_DIR = './resources/lang/qw'
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
+    
+    # Install translator
+    translator = QTranslator(app)
+    locale = sys.argv[1] if len(sys.argv) == 2 else 'en' 
+    translator.load('{}/lang_{}.qm'.format(LANG_DIR, locale))
+    app.installTranslator(translator)
+    
     window = duck_window()
     window.show()
     app.exec_()
