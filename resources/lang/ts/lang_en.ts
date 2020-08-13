b4c4b6e04644acd3214e95c99506cd96c1db66ef
Dmitry Ivanov <darcenrall@gmail.com>
2020-08-13 12:47:49 +0300

    add translation files

---
+++ resources/lang/ts/lang_en.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_design.py" line="308"/>
+        <source>Map Builder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="309"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="310"/>
+        <source>Map Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="311"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="312"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="313"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="314"/>
+        <source>Maps, signs, and objects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="315"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="316"/>
+        <source>Map editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="317"/>
+        <source>Default fill</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="318"/>
+        <source>Fill when deleted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="319"/>
+        <source>Set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="320"/>
+        <source>Open map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="321"/>
+        <source>Open *.yaml map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="322"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="323"/>
+        <source>Save map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="324"/>
+        <source>Save current map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="325"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="326"/>
+        <source>Export to PNG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="327"/>
+        <source>Ctrl+P</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="328"/>
+        <source>Calculate map characteristics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="329"/>
+        <source>Calculate map materials</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="330"/>
+        <source>Save map as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="331"/>
+        <source>Ctrl+Alt+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="332"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="333"/>
+        <source>Blocks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="334"/>
+        <source>Object description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="335"/>
+        <source>New map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="336"/>
+        <source>Create new map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="337"/>
+        <source>Ctrl+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="338"/>
+        <source>Map Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="339"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ resources/lang/ts/lang_ru.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_design.py" line="308"/>
+        <source>Map Builder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="309"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="310"/>
+        <source>Map Info</source>
+        <translation>Карты</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="311"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="312"/>
+        <source>View</source>
+        <translation>Вид</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="313"/>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="314"/>
+        <source>Maps, signs, and objects</source>
+        <translation>Карты, знаки и объекты</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="315"/>
+        <source>Description</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="316"/>
+        <source>Map editor</source>
+        <translation>Редактор карты</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="317"/>
+        <source>Default fill</source>
+        <translation>Заливка по умолчанию</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="318"/>
+        <source>Fill when deleted</source>
+        <translation>Заливка при удалении</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="319"/>
+        <source>Set</source>
+        <translation>Установить</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="320"/>
+        <source>Open map</source>
+        <translation>Открыть карту</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="321"/>
+        <source>Open *.yaml map</source>
+        <translation>Открыть карту в формате Yaml</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="322"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="323"/>
+        <source>Save map</source>
+        <translation>Сохранить карту</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="324"/>
+        <source>Save current map</source>
+        <translation>Сохранить текущую карту</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="325"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="326"/>
+        <source>Export to PNG</source>
+        <translation>Экпортировать в png</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="327"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="328"/>
+        <source>Calculate map characteristics</source>
+        <translation>Рассчитать характеристики</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="329"/>
+        <source>Calculate map materials</source>
+        <translation>Рассчитать материалы</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="330"/>
+        <source>Save map as</source>
+        <translation>Сохранить карту как</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="331"/>
+        <source>Ctrl+Alt+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="332"/>
+        <source>Exit</source>
+        <translation>Выйти</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="333"/>
+        <source>Blocks</source>
+        <translation>Блоки</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="334"/>
+        <source>Object description</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="335"/>
+        <source>New map</source>
+        <translation>Создать новую</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="336"/>
+        <source>Create new map</source>
+        <translation>Создать новую карту</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="337"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="338"/>
+        <source>Map Editor</source>
+        <translation>Редактор карт</translation>
+    </message>
+    <message>
+        <location filename="main_design.py" line="339"/>
+        <source>About</source>
+        <translation>Об авторах</translation>
+    </message>
+</context>
+</TS>
