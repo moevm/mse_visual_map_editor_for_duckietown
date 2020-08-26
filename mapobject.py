9a3095c53a75d9b8e1fc00031d43944c0b47d4ae
light5551 <sergey.glazunov.99@mail.ru>
2020-08-26 14:05:32 +0300

    add classes for objects from deffent layers

--- mapobject.py
+++ mapobject.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-class MapObject:
 
-    #position = {'x': 0, 'y': 0}
-    def __init__(self, kind, position=(0.0, 0.0), rotation=0, height=1, optional=False, static=True):
+class MapBaseObject:
+    def __init__(self, kind, position=(.0, .0), rotation=0, height=1, optional=False, static=True):
         self.kind = kind
         self.position = {}
         self.position['x'], self.position['y'] = position
@@ -10,3 +9,28 @@ class MapObject:
         self.height = height
         self.optional = optional
         self.static = static
+
+
+class SignObject(MapBaseObject):
+
+    def __init__(self, kind, **kwargs):
+        MapBaseObject.__init__(self, kind, **kwargs)
+
+
+class CityObject(MapBaseObject):
+
+    def __init__(self, kind, **kwargs):
+        MapBaseObject.__init__(self, kind, **kwargs)
+
+
+class WatchTowerObject(MapBaseObject):
+
+    def __init__(self, kind, **kwargs):
+        MapBaseObject.__init__(self, kind, **kwargs)
+        self.hostname = kwargs["hostname"]
+
+
+class GroundAprilTagObject(MapBaseObject):
+
+    def __init__(self, kind, **kwargs):
+        MapBaseObject.__init__(self, kind, **kwargs)    
