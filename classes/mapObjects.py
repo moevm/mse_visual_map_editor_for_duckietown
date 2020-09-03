62913462be5c468f177764da5f754160ca00b989
Dmitry Ivanov <darcenrall@gmail.com>
2020-09-03 13:52:16 +0300

    add get_editable_attrs for MapBaseObject classes

--- classes/mapObjects.py
+++ classes/mapObjects.py
@@ -5,8 +5,7 @@ from .baseClass import BaseEditorClass
 class MapBaseObject(BaseEditorClass):
     def __init__(self, init_info):
         BaseEditorClass.__init__(self, init_info)
-        self.position = {}
-        self.position['x'], self.position['y'] = init_info['pos']
+        self.position = {'x': init_info['pos'][0], 'y': init_info['pos'][1]}
         self.height = init_info['height']
         self.optional = init_info['optional']
         self.static = init_info['static']
@@ -20,6 +19,15 @@ class MapBaseObject(BaseEditorClass):
             'optional': self.optional,
             'static': self.static
         }.items()
+
+    def get_editable_attrs(self):
+        return {
+            'height': self.height,
+            'position': self.position,
+            'rotation': self.rotation,
+            'optional': self.optional,
+            'static': self.static
+        }
 
 
 class SignObject(MapBaseObject):
@@ -39,6 +47,14 @@ class WatchTowerObject(MapBaseObject):
     def __init__(self, init_info):
         MapBaseObject.__init__(self, init_info)
         self.hostname = init_info["hostname"] if "hostname" in init_info else "watchtower02" # TODO: How to init hostname?
+
+    def get_editable_attrs(self):
+        return {
+            'height': self.height,
+            'position': self.position,
+            'rotation': self.rotation,
+            'hostname': self.hostname
+        }
 
 
 class GroundAprilTagObject(MapBaseObject):
